9119e98d0926908e7350260e3db1a8edb26de33d
Dov_Dembin <Dov.Dembin@Dell.com>
2023-01-01 22:50:58 +0200

    ds

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -3,7 +3,19 @@ import groovy.io.*;
 
 @NonCPS
 def call(Map config=[:]) {
-    def dir = 10
+    def dir = new File(pwd());
 
-     
+    // new File(dir.path + '\\releasenotes.txt').withWriter('utf-8')
+    // { 
+    //     writer ->
+    //         dir.eachFileRecurse(FileType.ANY) { file ->
+    //             if (file.isDirectory() ) {
+    //                 writer.writeLine(file.name);
+    //             }
+    //             else
+    //             {
+    //                 writer.writeLine('\t' + file.name + '\t' + file.length());
+    //             }
+    //         }
+    // }
 }
